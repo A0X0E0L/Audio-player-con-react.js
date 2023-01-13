59264614f0993b78a9969086ec13e4501bf3dbda
A0X0E0L <ax13xxi@gmail.com>
2023-01-13 16:59:46 +0000

    Nueva modificacion de play audio con react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,66 +1,63 @@
-import React, {useState,useEffect} from "react";
-//include images into your bundle
-
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect, useRef} from "react";
 
 //create your first component
-
 const Home = () => {
-	const [SongURL, setSongURL] =useState([]);
+	const audioElement = useRef(null); //la const almacena una referencia que no tiene valor inicial
+	const [song, setSongs] =useState([]);
+
+function getSongs() { // funcion para recolectar y solicitar del servidor las canciones
+
+	fetch("https://assets.breatheco.de/apis/sound/songs")
+	.then(response => response.json()) // a la respuesta del servidor la convertimos en response.json
+	.then(data => {
+	(setSongs(data)) 
+		console.log(data); // data es el resultado de response.json
+	})
+
+}
 
 
+
+
+
+
+useEffect(() =>{ //al renderizar nos muestra un efecto
+	getSongs() 
+
+}, [])
+
+// Aquí hay un string, para agregarle otro(en este caso el "url" hay que ponerl el +)
+
+const playSong = (url) => { // funcion para reproducir canciones    
+	audioElement.current.src = "https://assets.breatheco.de/apis/sound/" + url
+	console.log(audioElement.current.src);
+	console.log(url);
 
 	
-
-	useEffect(()=>{
-		//codigo a ejecutar
-	console.log("La pagina se ha cargado exitosamente");
-	fetch("https://assets.breatheco.de/apis/sound/songs") //1.ir a buscar info en la url agrege
-	.then((response)=>response.json()) //2.Convierte la respuesta en un json
-	.then((data)=>setSongURL(data)) //3. Guarda el json en un objeto data
-
-	},[])
-	console.log(SongURL);
-
-	function ReproCancion() {
-		const [inputValue, setInputValue] = useState("");
-		const count = useRef(0);
-	  
-		useEffect(() => {
-		  count.current = count.current + 1;
-		});
-	  
-
-
+};
+	 
 	return (
-	       <>
- <div onChange={(e) => setInputValue(e.target.value)}/>
-      
+		<>
+		<h1>Usando UseEffect</h1>
+		<h2>Contador:</h2>
+{/* dibujamos la lista de canciones */}
+		<ul>
+			{song.map((oneSong, index)=><li onClick={ ()=>  playSong(oneSong.url)} key={oneSong.name}>{oneSong.name}</li>)}
+		</ul>
+		<div>
 
-			<ul>
-				{SongURL.map((item)=><li key={item.id}>{item.name}</li>)}
-			</ul>
-<div/>
-			<div>	
-<audio controls>
-  <source src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3" type="audio/ogg"/>
+		<audio controls src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3" ref={audioElement}>
+  <source type="audio/ogg"/>
   {/* <source src="horse.mp3" type="audio/mpeg"> */}
- 
+  Your browser does not support the audio element.
 </audio>
-
-		</div>
-       </>
-		);
-		
-		
-// 		<audio controls>
-//   <source src="horse.ogg" type="audio/ogg">
-//   <source src="horse.mp3" type="audio/mpeg">
-//   Your browser does not support the audio tag.
-// </audio>
+</div>
+		</>
+	);
 
 
 
-}};
+};
 
 export default Home;
+
